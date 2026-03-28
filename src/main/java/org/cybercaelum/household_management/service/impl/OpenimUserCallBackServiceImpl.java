733c2e6c93ce9e035ea78cc7453a2484a563ba8d
CyberCaelum <cybercaelum@163.com>
2026-03-28 21:35:34 +0800

    决定使用openim的回调功能维护客服的在线状态，

---
+++ src/main/java/org/cybercaelum/household_management/service/impl/OpenimUserCallBackServiceImpl.java
@@ -0,0 +1,13 @@
+package org.cybercaelum.household_management.service.impl;
+
+import org.cybercaelum.household_management.service.OpenimUserCallBackService;
+
+/**
+ * @author CyberCaelum
+ * @version 1.0
+ * @description: openim用户相关回调服务类
+ * @date 2026/3/28
+ */
+public class OpenimUserCallBackServiceImpl implements OpenimUserCallBackService {
+
+}
