dd3e4bad767139f62d35322b5ee3e6426bde9cff
潇泱 <148168907+lalelalelu@users.noreply.github.com>
2026-04-01 10:47:54 +0800

    修复了部分bug

--- src/main/java/org/cybercaelum/household_management/pojo/dto/QuitGroupDTO.java
+++ src/main/java/org/cybercaelum/household_management/pojo/dto/QuitGroupDTO.java
@@ -1,11 +1,20 @@
 package org.cybercaelum.household_management.pojo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author CyberCaelum
  * @version 1.0
  * @description: 让某个群成员退出指定群组
  * @date 2026/4/1 上午9:06
  */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class QuitGroupDTO {
     private String groupID;//群组id
     private String userID;//用户id
