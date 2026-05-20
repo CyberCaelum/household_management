4ff042576e93bd7ed55703f78880ab2df8e48a38
潇泱 <148168907+lalelalelu@users.noreply.github.com>
2026-05-20 14:14:55 +0800

    修改了拦截器范围

--- src/main/java/org/cybercaelum/household_management/config/WebMvcConfiguration.java
+++ src/main/java/org/cybercaelum/household_management/config/WebMvcConfiguration.java
@@ -41,6 +41,7 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
                 .addPathPatterns("/admin/**")  // 管理端也需要 JWT 认证
                 .addPathPatterns("/kefu/**")   // 客服端也需要 JWT 认证
                 .addPathPatterns("/order/**")  // 订单接口也需要 JWT 认证
+                .addPathPatterns("/group/**")
                 .excludePathPatterns(
                         "/user/login",
                         "/user/register",
